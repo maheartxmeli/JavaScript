c0a3b6bdafdd4fb19b2629c7603f6f87bc4350aa
maheartxmeli <117548903+maheartxmeli@users.noreply.github.com>
2023-01-12 22:39:39 -1000

    project1_expression_alert

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Aloha, E Komo Mai \\000/"); //this is to create a popup alert when first opening page
+
+var x = "\"aloha mai ka kou, "; //setting variable x with string
+
+document.write(x + "'o Mahea ko'u inoa.\" "); //calling write function
+
+var b = "Translation: " + "Hello all! My name is Mahea."; //setting variable b with string
+
+document.write(b); //calling write function 
+
+var one = " Where would you like to visit in Hawaii?", two = " What is your favorite island?"; //setting multiple variables
+
+document.write(two); //calling function 'two' to be written
+
+document.write(50 - 2); //calling an element in write function
---
+++ Project1_expressions_alert/JavaScript_File.js
@@ -0,0 +1,1 @@
+alert("Aloha Mai Ka Kou");
